22d0de66cfd11417b11271a40af7b7589355ebba
mkki <devmkki@gmail.com>
2025-05-17 21:20:32 +0900

    feat: BottomSheet 컴포넌트 1차 작업

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import PlaceCard from "../components/PlaceCard"; // 경로는 프로젝트 구조에 맞게 수정
-import "./HomePage.scss";
+import React from 'react';
+import PlaceCard from '../components/PlaceCard'; // 경로는 프로젝트 구조에 맞게 수정
+import './HomePage.scss';
+import BottomSheet from '../components/BottomSheet';
+import useBottomSheet from '../hooks/useBottomSheet';
 
 const HomePage = () => {
+  const { isOpen, open, close } = useBottomSheet();
+
   return (
     <div className="home-page">
       홈페이지입니다.
@@ -12,6 +16,12 @@ const HomePage = () => {
         <PlaceCard title="도로 안전" likes={127} status="solved" />
         <PlaceCard title="무단 투기" likes={127} status="solved" />
       </div>
+      <button onClick={open}>바텀 시트 열기</button>
+      <BottomSheet isOpen={isOpen} onClose={close}>
+        <h2>바텀 시트</h2>
+        <p>내용을 여기에 넣으세요.</p>
+        <button onClick={close}>닫기</button>
+      </BottomSheet>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import svgr from 'vite-plugin-svgr';
+import path from 'node:path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   css: {
     postcss: {
       plugins: [autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
