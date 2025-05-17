aca8dd9c3ca27f2a03af28d428589afef93a25cb
mkki <devmkki@gmail.com>
2025-05-18 01:06:40 +0900

    feat: 구글맵 컴포넌트 추가
    - GPS 정보 기반 현재 위치 지도 노출

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "@/pages/HomePage";
+import GoogleMap from "@/components/Map/GoogleMap";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <GoogleMap />
       <main className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
